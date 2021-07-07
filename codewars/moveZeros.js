bc20cd41402f991247b42d0821296f5bdae60e3a
vladimirkhvan <vladimirkhvan23@gmail.com>
2021-07-07 21:14:16 +0500

    new funcs

---
+++ codewars/moveZeros.js
@@ -0,0 +1,18 @@
+var moveZeros = function (arr) {
+  
+    let counter = 0;
+    
+    for(let i = 0; i<arr.length; i++){
+      if(arr[i] === 0){
+        counter++;
+      }
+    }
+    
+    arr = arr.filter(element => element !== 0);
+    
+    for(let i = 0; i<counter; i++){
+      arr.push(0);
+    }
+    
+    return arr;
+  }
--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 window.onload = function(){
     
-
+    console.log(readNumber(3));
 
 }
 
 
+// NUMBERS WITH PRECISION ISSUES
+
+    // function sumFloat(){
+    //     let number1 = +prompt("Input 1st number", 3);
+    //     let number2 = +prompt("Input 2st number", 3);
+
+    //     return number1.toFixed(10) + number2.toFixed (10);
+    // }
+
+    // function readCertainNumber(n){
+    //     let a = prompt("Input value");
+    //     while( a != null && +a != n ){  }
+    //     return a;
+    // }
+
+    // function readNumber(){
+    //     let a;
+    //     while( !isFinite(a = prompt("Input value")) ){  };
+    //     if(a == null){
+    //         return null;
+    //     }
+    //     return a;
+    // }
+
+    // function random(min, max){
+    //     return (Math.random() * (max - min) + min);
+    // }
+
+    // function randomInt(min, max){
+    //     return Math.floor(Math.random() * (max + 1 - min) + min);
+    // }
 
 
 // constuctor for accumulating some values
@@ -66,7 +97,7 @@ window.onload = function(){
 //     }
     
 //     return result;
-// }
+// }    
 
 // function pow(number, n){
 //     let result = 1;
