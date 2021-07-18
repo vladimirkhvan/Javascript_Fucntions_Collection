8079ceb20fb39bdc5abbaacffac780ba02f67b90
vladimirkhvan <vladimirkhvan23@gmail.com>
2021-07-18 22:43:31 +0500

    5 kyu

--- codewars/rot13.js
+++ codewars/rot13.js
@@ -1,6 +1,24 @@
-function rot13(message) {
-    return message.slice("").map(char => {
-        char.fromCodePoint(i + 13)
+function rot13(message){
+  
+    let doubleAlphabet = [];
+    
+    for (let i = 65; i <= 90; i++) {
+      doubleAlphabet.push(String.fromCodePoint(i));
     }
-    )
-}
+    for (let i = 65; i <= 90; i++) {
+      doubleAlphabet.push(String.fromCodePoint(i));
+    }
+    for (let i = 97; i <= 122; i++) {
+      doubleAlphabet.push(String.fromCodePoint(i));
+    }
+    for (let i = 97; i <= 122; i++) {
+      doubleAlphabet.push(String.fromCodePoint(i));
+    }
+    
+    return message.split("").map(char => {
+      if(doubleAlphabet.lastIndexOf(char) != -1){
+        return doubleAlphabet[doubleAlphabet.lastIndexOf(char)-13]
+      }
+      return char;
+    }).join("");
+  }
