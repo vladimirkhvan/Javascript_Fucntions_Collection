91d9a48f636890639ae55d0eea7f0a365bd3070c
vladimirkhvan <vladimirkhvan23@gmail.com>
2021-08-02 19:30:53 +0500

    promisify callback

---
+++ learn js/promisify-callback.js
@@ -0,0 +1,23 @@
+// promisify(f, true), чтобы получить массив результатов
+function promisify(f, manyArgs = false) {
+    return function (...args) {
+      return new Promise((resolve, reject) => {
+        function callback(err, ...results) { // наш специальный колбэк для f
+          if (err) {
+            return reject(err);
+          } else {
+            // делаем resolve для всех results колбэка, если задано manyArgs
+            resolve(manyArgs ? results : results[0]);
+          }
+        }
+  
+        args.push(callback);
+  
+        f.call(this, ...args);
+      });
+    };
+  };
+  
+  // использование:
+  f = promisify(f, true);
+  //f(...).then(arrayOfResults => ..., err => ...)
