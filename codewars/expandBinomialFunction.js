389cd89d625a8a4d8b3d376c33d3f6df49ddbf67
vladimirkhvan <vladimirkhvan23@gmail.com>
2021-08-03 20:48:31 +0500

    3 kyu should be finished

--- codewars/expandBinomialFunction.js
+++ codewars/expandBinomialFunction.js
@@ -1,34 +1,37 @@
 function expand(expr) {
     let expression = expr.split("^")[0];
     let degree = expr.split("^")[1];
+    console.log(expression);
+    console.log(degree);
 
-    expression = expression.split("").pop().shift();
+    expression = expression.split("");
+    expression.pop();
+    expression.shift();
+    console.log(expression);
 
     let a, x, b, sign;
 
 
 
-    console.log(expression);
-    console.log(degree);
 }
 
 function multiply(a, b){
-
+    
 }
 
 function sum(a, b){
 
 }
 
-function hash(coefficient, power){
-
+function hash(coefficient, power, x){
+    return coefficient.toString() + x + "^" + power;
 }
 
 function dehash(string, coefficient, power){
 
-    power = string.split("^")[1];
+    power = +string.split("^")[1];
 
-    coefficient = +string.split("^")[0].filter(item => typeof(item) == "number").join("");
+    coefficient = string.split("^")[0].match(/(\d+)/)[0];
 
     if(coefficient[0] == "-"){
         coefficient *= -1;
