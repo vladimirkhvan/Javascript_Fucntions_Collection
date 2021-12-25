d6f0994bb00df6bb931766dbd880f80deaa4c9e7
vladimirkhvan <vladimirkhvan23@gmail.com>
2021-12-26 01:11:11 +0500

    7kyu

--- codewars/zombieOnslaught.js
+++ codewars/zombieOnslaught.js
@@ -1,25 +1,25 @@
-    function zombie_shootout (zombies, range, ammo) {
-    
-        console.log(`zombies: ${zombies}`)
-        console.log(`range: ${range}`)
-        console.log(`ammo: ${ammo}`)
-        
-        let possibleShots = range/0.5;
-        let killedZombies =  possibleShots > ammo ? ammo : possibleShots;
-        
-        if(ammo>=zombies && possibleShots >= zombies){
-        
+function zombie_shootout(zombies, range, ammo) {
+
+    console.log(`zombies: ${zombies}`)
+    console.log(`range: ${range}`)
+    console.log(`ammo: ${ammo}`)
+
+    let possibleShots = range / 0.5;
+    let killedZombies = possibleShots > ammo ? ammo : possibleShots;
+
+    if (ammo >= zombies && possibleShots >= zombies) {
+
         killedZombies = zombies;
-        
+
         return `You shot all ${killedZombies} zombies.`;
-        
-        }
-        
-        killedZombies =  possibleShots > ammo ? ammo : possibleShots;
-        
-        if(ammo>=possibleShots && ammo <= zombies){
-        return `You shot ${killedZombies} zombies before being eaten: overwhelmed.`;
-        }
-        
-        return `You shot all ${killedZombies} zombies.`;
+
     }
+
+    killedZombies = possibleShots > ammo ? ammo : possibleShots;
+
+    if (ammo >= possibleShots && ammo <= zombies) {
+        return `You shot ${killedZombies} zombies before being eaten: overwhelmed.`;
+    }
+
+    return `You shot all ${killedZombies} zombies.`;
+}
